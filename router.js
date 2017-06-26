ad21e89ca24af305ae5fe4c728d999d013cb61e1
Max Hf <Max@Macbooks-MacBook-Air.local>
2017-06-26 13:50:41 +0200

    ajax form anfrage implementiert

--- router.js
+++ router.js
@@ -24,7 +24,13 @@ router.get('/quest', function(req, res){
 router.get('/something', function(req, res){
     var db = req.db;
     var collection = db.get('quests');
-    collection.find({}, function(err, quests){
+    var questForm = req.query.questForm;
+    questForm = questForm.split("");
+    for(var i = 0; i < questForm.length; i++){
+        if(questForm[i] == 1) questForm[i] = true;
+        else questForm[i] = false;
+    }
+    collection.find({'outdoor': questForm[0], 'sport': questForm[1]}, function(err, quests){
         if (err) {
             res.send('much error');
         }
